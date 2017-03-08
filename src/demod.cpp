dff52b37dfb1c5e0320c6048177f1f4c634c2acb
Charlie FG <cfg@sonicflops.org>
2017-03-08 12:40:13 +0000

    Add hanning window before spectral analysis

--- src/demod.cpp
+++ src/demod.cpp
@@ -5,8 +5,10 @@
 #include "jack_client.hpp"
 #include "sync.hpp"
 
-#define THRESHOLD 8
+#define THRESHOLD 20
+#define CONTROL_THRESHOLD 10
 #define CONTROL_RESET 80
+#define BOOSTER 1
 
 
 #define FS 44100.0f
@@ -99,13 +101,13 @@ struct magnitudes {
 };
 
 void handle_zero(magnitudes& mag, payload& pl) {
-  if(mag.f0a > 100) {
+  if(mag.f0a > THRESHOLD) {
     if(pl.ctl_marker != 0) {
       pl.data_buffer[pl.bit_num] = 0;
       pl.bit_num++;
     }
     pl.ctl_marker = 0;
-  } else if(mag.f0b > 100) {
+  } else if(mag.f0b > THRESHOLD) {
     if(pl.ctl_marker != '0') {
      pl.data_buffer[pl.bit_num] = 0;
      pl.bit_num++;
@@ -115,13 +117,13 @@ void handle_zero(magnitudes& mag, payload& pl) {
 }
 
 void handle_one(magnitudes& mag, payload& pl) {
-  if(mag.f1a > 100) {
+  if(mag.f1a > THRESHOLD) {
     if(pl.ctl_marker != 1) {
       pl.data_buffer[pl.bit_num] = 1;
       pl.bit_num++;
     }
     pl.ctl_marker = 1;
-  } else if(mag.f1b > 100) {
+  } else if(mag.f1b > THRESHOLD) {
     if(pl.ctl_marker != '1') {
       pl.data_buffer[pl.bit_num] = 1;
       pl.bit_num++;
@@ -148,14 +150,13 @@ std::string decode_bin(std::uint8_t* bin, unsigned int num_bits) {
 
 void resolve_state(magnitudes& mag, payload& pl) {
 
-  if(mag.f0a > 100 | mag.f0b > 100) {
+  if(mag.f0a > THRESHOLD | mag.f0b > THRESHOLD) {
     handle_zero(mag, pl);
-  } else if(mag.f1a > 100 || mag.f1b > 100) {
+  } else if(mag.f1a > THRESHOLD || mag.f1b > THRESHOLD) {
     handle_one(mag, pl);
-  } else if(mag.fc > 400) {
+  } else if(mag.fc > CONTROL_THRESHOLD) {
     pl.ctl_marker = 'c';
     if(pl.bit_num > 0) {
-      
       std::stringstream s;
       for(auto b = 0u; b < pl.bit_num; b++) {
         s << std::to_string(pl.data_buffer[b]);
@@ -163,12 +164,26 @@ void resolve_state(magnitudes& mag, payload& pl) {
       
       std::cout << "read " << pl.bit_num << " bits\n";
       std::cout << "\n" << s.str() << "\n";
-      std::cout << "\n\n" << decode_bin(pl.data_buffer, pl.bit_num) << std::endl;
+      std::cout << "\n\n" << decode_bin(pl.data_buffer, pl.bit_num) << "\n\n\n\n\n\n" << std::endl;
     }
     pl.bit_num = 0;
   } 
 }
 
+inline float window_hanning(unsigned int n, unsigned int N) {
+  constexpr float pi2 = 2 * M_PI;
+  return 0.5f - (0.5f * cosf( pi2 * n / N));
+}
+
+void print_magnitudes(magnitudes& mag) {
+    std::cout << "\e[A\e[A\e[A\e[A\e[A" 
+      <<               "fc:      " << mag.fc 
+      << "            \nf0a:      " << mag.f0a
+      << "            \nf0b:      " << mag.f0b
+      << "            \nf1a:      " << mag.f1a
+      << "            \nf1b:      " << mag.f1b
+      << "            \n" << std::flush;
+  };
 int process(jack_nframes_t nframes, void* data) {
   auto pl = static_cast<payload*>(data);
   
@@ -189,6 +204,7 @@ int process(jack_nframes_t nframes, void* data) {
 
   for(auto i = 0u; i < nframes; i++) {
     auto filtered = filter(in_buffer[i], pl->taps, pl->sample_buffer, NUM_TAPS);
+    filtered *= window_hanning(i, nframes);
     pl->output_buffer[i] =  filtered;
     pl->fftw.in[i] = filtered;
   }
@@ -201,22 +217,15 @@ int process(jack_nframes_t nframes, void* data) {
   auto fc  = pl->fftw.out[ pl->fftw.bins[FREQ::CONTROL] ][0];
   auto f1a = pl->fftw.out[ pl->fftw.bins[FREQ::ONEA] ][0];
   auto f1b = pl->fftw.out[ pl->fftw.bins[FREQ::ONEB] ][0];
-  mag.f0a = f0a < 0.0 ? f0a * -1.0 : f0a;
-  mag.f0b = f0b < 0.0 ? f0b * -1.0 : f0b;
-  mag.f1a = f1a < 0.0 ? f1a * -1.0 : f1a;
-  mag.f1b = f1b < 0.0 ? f1b * -1.0 : f1b;
+  mag.f0a = (f0a < 0.0 ? f0a * -1.0 : f0a) * BOOSTER;
+  mag.f0b = (f0b < 0.0 ? f0b * -1.0 : f0b) * BOOSTER;
+  mag.f1a = (f1a < 0.0 ? f1a * -1.0 : f1a) * BOOSTER;
+  mag.f1b = (f1b < 0.0 ? f1b * -1.0 : f1b) * BOOSTER;
   mag.fc  = fc < 0.0 ? fc * -1.0 : fc;
   
   resolve_state(mag, *pl);
 
-  auto print_fs = [&](float f0, float f1, float f2) {
-    std::cout << "\e[A\e[A\e[A\e[A" 
-      <<               "fc:      " << fc 
-      << "            \nf0:      " << f0
-      << "            \nf1:      " << f1
-      << "            \n" << pl->ctl_marker
-      << "            \n" << std::flush;
-  };
+  print_magnitudes(mag); 
 
   return 0;
 }
@@ -224,7 +233,6 @@ int process(jack_nframes_t nframes, void* data) {
 
 
 int main(int argc, char** argv) {
-    std::cout << "Running demodulator...\n\n\n\n";
     sfsw::jack::client client("wivi-demod");
    
     payload pl;
@@ -233,18 +241,24 @@ int main(int argc, char** argv) {
     pl.ctl_marker = 'w';
     pl.data_buffer = new std::uint8_t[1024];
     
-    auto binwidth = FS / 2048;
+    constexpr int binwidth = FS / 2048;
         
     pl.fftw.bins[FREQ::CONTROL] = static_cast<unsigned int>( FC0 / binwidth);
     pl.fftw.bins[FREQ::ZEROA] = static_cast<unsigned int>( F0A / binwidth);
     pl.fftw.bins[FREQ::ONEA] = static_cast<unsigned int>( F1A / binwidth);
     pl.fftw.bins[FREQ::ZEROB] = static_cast<unsigned int>( F0B / binwidth);
     pl.fftw.bins[FREQ::ONEB] = static_cast<unsigned int>( F1B / binwidth);
-    pl.taps = bpf(NUM_TAPS,44.1,0.2,0.9);
+    pl.taps = bpf(NUM_TAPS,44.1,0.3,0.9);
     pl.sample_buffer = create_buffer(NUM_TAPS);
-    
-    for(auto i =0; i < 5; i++){ std::cout << pl.fftw.bins[i] << " "; }
-    std::cout << std::endl;
+
+    std::cout << "fc : " << FC0 << "Hz\t" << "(" << pl.fftw.bins[FREQ::CONTROL] << ")\n";
+    std::cout << "f0a: " << F0A << "Hz\t" << "(" << pl.fftw.bins[FREQ::ZEROA] << ")\n";
+    std::cout << "f0b: " << F0B << "Hz\t" << "(" << pl.fftw.bins[FREQ::ZEROB] << ")\n";
+    std::cout << "f1a: " << F1A << "Hz\t" << "(" << pl.fftw.bins[FREQ::ONEA] << ")\n";
+    std::cout << "f1b: " << F1B << "Hz\t" << "(" << pl.fftw.bins[FREQ::ONEB] << ")\n";
+    std::cout << "Waiting for signal...\n\n\n\n\n" << std::endl;    
+
+
     pl.output_buffer = nullptr;
     
     pl.fftw.in = nullptr;
--- src/sync.hpp
+++ src/sync.hpp
@@ -3,21 +3,21 @@
 #define SYNC_HPP
 
 #define BIT_WIDTH 1
-#define F0A 240
-#define F0B 120
-
-#define FC0  480
-
-#define F1A 720
-#define F1B 940
-
-//#define F0A 510
-//#define F0B 610
+//#define F0A 240
+//#define F0B 120
 //
-//#define FC0  300
+//#define FC0  480
 //
-//#define F1A 710
-//#define F1B 810
+//#define F1A 720
+//#define F1B 940
+
+#define FC0  473
+
+#define F0A 536
+#define F0B 599
+
+#define F1A 662
+#define F1B 725
 
 #endif /* SYNC_HPP */
 
